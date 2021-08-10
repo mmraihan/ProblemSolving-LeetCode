4633baa929e93e30e078c07556b6ee38161a6995
mmraihan <mm_raihan@live.com>
2021-08-11 04:41:30 +0600

    Swap Two Numbers

---
+++ SwapTwoNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SwapTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int temp;
+
+            Console.WriteLine("Before swapping value of a={0}, b={1}", a,b);
+
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After swapping value of a={0}, b={1}", a, b);
+
+            //int temp;
+
+            //temp = a;
+            //a = b;
+
+
+
+        }
+    }
+}
