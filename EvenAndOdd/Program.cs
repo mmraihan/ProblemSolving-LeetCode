e5f3a8dcbdecbe8b6a91d5658de8385c706fbbcc
mmraihan <mm_raihan@live.com>
2021-08-11 03:41:41 +0600

    Even and Odd Using Continue

---
+++ EvenAndOdd/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EvenAndOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <=100; i++)
+            {
+                if (i % 2 ==1)
+                {
+                    continue;
+                }             
+               Console.Write(i + ",");
+
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                Console.Write(i + ",");
+
+            }
+        }
+    }
+}
