0e1eb926ab06b07607fe7ecfd14df95a81f73ae6
mmraihan <mm_raihan@live.com>
2021-08-25 21:44:01 +0600

    Summation of a given Number

---
+++ Logical-Programs/1. SumOfDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1._SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Provide input numbers:");
+            int inputNumber = Int32.Parse( Console.ReadLine());
+
+            int backupValue = inputNumber;
+            int sum = 0, remainder = 0;
+
+            while (inputNumber !=0)
+            {
+                remainder = inputNumber % 10;
+                sum += remainder;
+                remainder = inputNumber / 10;
+                inputNumber /= 10;
+            }
+            Console.WriteLine($"Sum of {backupValue} is: {sum}");
+
+        }
+    }
+}
