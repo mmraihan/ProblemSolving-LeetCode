654dd900ee82e6011ba5cde212e50ba4fbd189f2
mmraihan <mm_raihan@live.com>
2021-08-26 19:05:09 +0600

    4. Multiplication table for a given range

---
+++ Logical-Programs/4. MultiplicationTableWithRange/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4._MultiplicationTableWithRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Provide input number to calculate multiplicaton table with in range: ");
+
+            int inputRange = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i <= inputRange; i++)
+            {
+                for (int j = 1; j <11; j++)
+                {
+                    Console.Write($"{i}*{j}={i*j}"+ "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
