8e829c64268e99da7903fdc59478e1f30f4023c5
mmraihan <mm_raihan@live.com>
2021-08-27 17:02:15 +0600

    20. Valid Parentheses

---
+++ 5.  ValidParentheses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _5.__ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValid("{}))"));
+        }
+
+
+        public static bool IsValid(string s)
+        {
+            if (s.Length % 2 !=0)
+            {
+                return false;
+            }
+
+            Stack<char> stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (((c == ')' && stack.Count > 0 && stack.Peek() == '(')) ||
+                        ((c == '}' && stack.Count > 0 && stack.Peek() == '{')) ||
+                        ((c == ']' && stack.Count > 0 && stack.Peek() == '[')))
+                    stack.Pop();
+
+                else
+                
+                    stack.Push(c);
+                
+                        
+            }
+
+            return stack.Count == 0 ? true : false;
+        }
+    }
+}
