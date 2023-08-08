9f13da1a1761827cecd472ce232e91f331595fb0
mmraihan <mm_raihan@live.com>
2023-08-09 00:10:10 +0600

    Remove duplicate value

---
+++ Remove-DupllicateValue/Program.cs
@@ -0,0 +1,23 @@
+﻿var arr = new int[] {1,2,3,3,4,4,4,5,6,6,7,7,8,8};
+
+for (int i = 0; i < arr.Length; i++)
+{
+	for (int j = i+1; j < arr.Length; j++)
+	{
+		if (arr[i] == arr[j])
+		{
+			arr[i] = 3;
+		}
+	}
+}
+Console.WriteLine("Result");
+
+for (int i = 0; i < arr.Length; i++)
+{
+	if (arr[i]!=3)
+	{
+		Console.WriteLine(arr[i]);
+	}
+}
+
+Console.ReadLine();
