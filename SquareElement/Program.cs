ceb7e104087141a47431f77faf80cfdda5165f43
mmraihan <mm_raihan@live.com>
2023-08-09 02:45:41 +0600

    SquareElement.

---
+++ SquareElement/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int[] input = { 1, 2, 3 };
+int[] output = SquareElements(input);
+
+Console.Write(string.Join(",",output));
+
+Console.WriteLine();
+
+
+
+static int[] SquareElements(int[] input)
+{
+    int[] output = new int[input.Length];
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        output[i] = input[i] * input[i];
+    }
+
+    return output;
+}
