4e4249c3c86a9a62a934956dd04f1f0f4b2987f0
mmraihan <mm_raihan@live.com>
2023-08-09 01:06:12 +0600

    Reverse String.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ReverseString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Enter String: ");
+            var str = Console.ReadLine();
+
+            var reverseString =GetReverseString(str);
+  
+            Console.WriteLine($"Reverse string is : {reverseString}");
+            Console.ReadLine();     
+        }
+
+
+        public static string GetReverseString( string str)
+        {
+            var revString = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                revString = revString + str[i];
+            }
+
+            return revString;
+        }
+
+
+    }
+}
