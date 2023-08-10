637b90847a0a4d2034b62c0493cec2bed1f590b3
mmraihan <mm_raihan@live.com>
2023-08-11 01:03:25 +0600

    MaxMinValueInArray

---
+++ MaxMinValueInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MaxMinValueInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int[] numbers = { 1, 2, 3,44,55,66,79,66 };
+          
+            int minValue = numbers[0];
+            int maxValue = numbers[0];
+
+            foreach (var item in numbers)
+            {
+                if (item<minValue)
+                {
+                    minValue = item;
+                }
+                if (item>maxValue) 
+                {
+                    maxValue = item;
+                }
+            }
+            Console.WriteLine($"Minimum Value: {minValue}");
+            Console.WriteLine($"Maximun Value: {maxValue}");
+
+        }
+
+      
+    }
+}
---
+++ boolean/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.Metrics;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace boolean
+{
+    #region Return true
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5; int y = 3;
+
+            var result = CheckCondition(x, y);
+            Console.WriteLine(result);
+
+        }
+
+        static bool CheckCondition(int x, int y)
+        {
+            return x > y;
+        }
+    }
+    #endregion
+
+
+}
+   
+    
