1060be8850fe911fd78fcc6ca12e91552c578743
mmraihan <mm_raihan@live.com>
2024-05-26 17:34:27 +0600

    Merge Strings Alternately

---
+++ 1768-Merge-Strings-Alternately/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+namespace _1768_Merge_Strings_Alternately
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        { 
+            var result= MergeAlternately("abc", "pqr");
+            Console.WriteLine(result);
+        }
+
+        public  static string MergeAlternately(string word1, string word2)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int n1 = word1.Length;
+            int n2 = word2.Length;
+            int i = 0;
+            while (i < n1 || i < n2)
+            {
+                if (i < n1)
+                {
+                    stringBuilder.Append(word1[i]);
+                   
+                }
+                if (i < n2)
+                {
+                    stringBuilder.Append(word2[i]);
+                }
+                i++;
+            }
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
