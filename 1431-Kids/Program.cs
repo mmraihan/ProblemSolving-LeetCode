7a02f8707c57bc4bfbf88ee8f31dada812764d4b
mmraihan <mm_raihan@live.com>
2024-06-11 15:20:45 +0600

    605

---
+++ 1431-Kids/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _1431_Kids
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputA = [2, 3, 5, 1, 3];
+            var inputB = 3;
+
+            var result = KidsWithCandies(inputA, inputB);
+            foreach ( var i in result ) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+
+        static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            var result = new List<bool>();
+
+            for (int i = 0; i < candies.Length; i++)
+            {
+                bool test = false;
+                for (int j = 0; j <candies.Length ; j++)
+                {
+                    if (candies[i] + extraCandies < candies[j])
+                    {
+                        test = true;
+                        break;
+                    }
+                }
+                if (test)
+                {
+                    result.Add(false);
+                }
+                else
+                {
+                    result.Add(true);
+                }              
+            }
+            return result;
+        }
+        
+    }
+}
---
+++ 605-Can-Place-Flowers/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _605_Can_Place_Flowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] flowerbed1 = { 1, 0, 0, 0, 1 };
+            int n1 = 1;
+            Console.WriteLine(CanPlaceFlowers(flowerbed1, n1));
+
+            int[] flowerbed2 = { 1, 0, 0, 0, 1 };
+            int n2 = 2;
+            Console.WriteLine(CanPlaceFlowers(flowerbed2, n2)); 
+        }
+
+        static bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            int count = 0;
+            int length = flowerbed.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (flowerbed[i] == 0)
+                {
+                    bool emptyPrev = (i == 0) || (flowerbed[i - 1] == 0);
+                    bool emptyNext = (i == length - 1) || (flowerbed[i + 1] == 0);
+
+                    if (emptyPrev && emptyNext)
+                    {
+                        flowerbed[i] = 1;
+                        count++;
+
+                        if (count >= n)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return count >= n;
+        }
+    }
+}
